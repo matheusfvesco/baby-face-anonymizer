9645ea5935338f5d5a8658f407596e03497f9c4c
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2025-04-02 01:53:33 +0000

    Clear comments

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -1,7 +1,5 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
+model = YOLO("yolo11n.pt") 
 
-# Train the model
 results = model.train(data="baby_anonymizer.yaml", epochs=100, imgsz=640)
