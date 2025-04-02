1d508fd285129b44d27aa1897edcd290a6d764c1
matheusfvesco <114014793+matheusfvesco@users.noreply.github.com>
2025-04-02 21:32:13 +0000

    Add train

--- run_pipe.sh
+++ run_pipe.sh
@@ -3,3 +3,5 @@ python src/scripts/mine.py data/queries.txt data/mined
 python src/scripts/annotate.py data/mined data/annots
 
 python src/scripts/split.py data/mined data/annots output/data
+
+python src/scripts/train.py
